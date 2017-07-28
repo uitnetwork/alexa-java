8c802f30556410eb0351ae8741545417700b997c
Ninh Doan <uitnetwork@gmail.com>
2017-07-28 23:27:03 +0800

    First init for hello world to alexa using Java

---
+++ src/main/java/com/uitnetwork/alexa/speechlet/HelloWorldSpeechlet.java
@@ -0,0 +1,68 @@
+package com.uitnetwork.alexa.speechlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.*;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+
+public class HelloWorldSpeechlet implements SpeechletV2 {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
+
+    @Override
+    public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
+        logger.info("onSessionStarted requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(), requestEnvelope.getSession().getSessionId());
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> requestEnvelope) {
+        logger.info("onLaunch requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(), requestEnvelope.getSession().getSessionId());
+        return getWelcomeResponse();
+    }
+
+    private SpeechletResponse getWelcomeResponse() {
+        PlainTextOutputSpeech plainTextOutputSpeech = new PlainTextOutputSpeech();
+        plainTextOutputSpeech.setText("Welcome to the Alexa Skills Kit, you can say hello");
+
+        PlainTextOutputSpeech repromptOutputSpeech = new PlainTextOutputSpeech();
+        repromptOutputSpeech.setText("Please say something");
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptOutputSpeech);
+
+        return SpeechletResponse.newAskResponse(plainTextOutputSpeech, reprompt);
+    }
+
+    @Override
+    public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
+        logger.info("onIntent requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(), requestEnvelope.getSession().getSessionId());
+
+        Intent intent = requestEnvelope.getRequest().getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        if ("HelloWorldIntent".equals(intentName)) {
+            return getHelloResponse();
+        } else {
+            throw new RuntimeException("Invalid Intent");
+        }
+    }
+
+    private SpeechletResponse getHelloResponse() {
+        PlainTextOutputSpeech plainTextOutputSpeech = new PlainTextOutputSpeech();
+        plainTextOutputSpeech.setText("just an hello");
+
+        PlainTextOutputSpeech repromptOutputSpeech = new PlainTextOutputSpeech();
+        repromptOutputSpeech.setText("Nothing is said");
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptOutputSpeech);
+
+        return SpeechletResponse.newAskResponse(plainTextOutputSpeech, reprompt);
+    }
+
+    @Override
+    public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> requestEnvelope) {
+        logger.info("onSessionEnded requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(), requestEnvelope.getSession().getSessionId());
+    }
+}
---
+++ src/main/java/com/uitnetwork/alexa/util/AppConstants.java
@@ -0,0 +1,6 @@
+package com.uitnetwork.alexa.util;
+
+public interface AppConstants {
+
+    String SUPPORTED_APP_ID = "amzn1.ask.skill.a2a408a0-abae-46d5-9668-b5f6ebf8d855";
+}
